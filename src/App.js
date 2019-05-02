16b95356eb37edbeefad2c7821e6cef3d9a6cdac
Ashwin Bordoloi <ashwin1014@yahoo.co.in>
2019-05-02 18:09:57 +0530

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Button, Row, Col } from 'antd';
 import './App.css';
 
-function App() {
+
+const handleClick = () => {
+  alert('Hello World')
+}
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <Row type="flex" justify="center">
+            <Col span={24}>
+              <Button type="primary">Click Me!</Button>
+            </Col>
+        </Row>        
+      </div>
     </div>
   );
-}
+};
 
 export default App;
